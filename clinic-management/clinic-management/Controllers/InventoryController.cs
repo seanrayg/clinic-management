1b17d08600f522462f0c3605a609f3ce942c49b2
Sean Garcia <15.seangarcia@gmail.com>
2018-01-04 18:15:34 +0800

    WIP: Inventory

---
+++ clinic-management/clinic-management/Controllers/InventoryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using clinic_management.Models;
+
+namespace clinic_management.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private dbClinicManagementEntities db = new dbClinicManagementEntities();
+
+        // GET: Inventory
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
