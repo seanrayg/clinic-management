5e9f888613f932cab6f14543c5574b058925fc00
Sean Garcia <15.seangarcia@gmail.com>
2018-01-26 15:52:53 +0800

    WIP: Medical Check

---
+++ clinic-management/clinic-management/Controllers/MedChecksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace clinic_management.Controllers
+{
+    public class MedChecksController : Controller
+    {
+        // GET: MedChecks
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
