dfd8c7bfea86462d0eea2510a6458d47db9cd323
Jonina Fontanilla <onipampam@gmail.com>
2018-02-28 09:03:28 +0800

    Revert "Revert "Merge branch 'master' into nina-new""
    This reverts commit ef915722f59b7b040d57a333d6ed49cca5f05aca.

---
+++ clinic-management/clinic-management/Models/ModelExpiredItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace clinic_management.Models
+{
+    public class ModelExpiredItem
+    {
+        public string ItemID { get; set; }
+        public string ItemName { get; set; }
+        public int Count { get; set; }
+    }
+}
